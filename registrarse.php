71b180e47ffec69e4094e8196c649f8df187d095
dan <diazdaniel422@gmail.com>
2021-05-04 14:28:10 -0500

    Registro terminado
    Registro añadido!!!!

--- registrarse.php
+++ registrarse.php
@@ -1,22 +1,125 @@
 <?php
-    require 'includes/app.php';
-    
-    $db = conectarDB();
-    $errores = [];
+require 'includes/app.php';
 
+$db = conectarDB();
+$errores = [];
+#iniciamos las variables para que no se borren al momento de ingresar datos erroneos
 
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        #filtrado de los datos
-        $email = mysqli_real_escape_string($db, filter_var($_POST['correo'], FILTER_VALIDATE_EMAIL));
-        $contrasenia = mysqli_real_escape_string($db, $_POST['contrasenia']);
-        #$email = mysqli_real_escape_string($db, filter-var($_POST['']));
+$correo = '';
+$contrasenia = '';
+$nombre = '';
+$apMaterno = '';
+$apPaterno = '';
+$telefono  = '';
+$estado = '';
+$colonia = '';
+$calle = '';
+$numero = '';
+$cp = '';
+$referencias = '';
+$ciudad = '';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    #filtrado de los datos
+    $correo = mysqli_real_escape_string($db, filter_var($_POST['correo'], FILTER_VALIDATE_EMAIL));
+    $contrasenia = mysqli_real_escape_string($db, $_POST['contrasenia']);
+    $nombre = mysqli_real_escape_string($db, $_POST['name']);
+    $apMaterno = mysqli_real_escape_string($db, $_POST['apMaterno']);
+    $apPaterno = mysqli_real_escape_string($db, $_POST['apPaterno']);
+    $telefono  = mysqli_real_escape_string($db, $_POST['telefono']);
+    $estado = mysqli_real_escape_string($db, $_POST['estado']);
+    $colonia = mysqli_real_escape_string($db, $_POST['colonia']);
+    $calle = mysqli_real_escape_string($db, $_POST['calle']);
+    $numero = mysqli_real_escape_string($db, $_POST['numero']);
+    $cp = mysqli_real_escape_string($db, $_POST['cp']);
+    $referencias = mysqli_real_escape_string($db, $_POST['referencias']);
+    $ciudad = mysqli_real_escape_string($db, $_POST['ciudad']);
+    $pais = mysqli_real_escape_string($db, $_POST['pais']);
+    #preparamos una consulta para verificar si el correo no se ha registrado ya dos veces
+    $query = "SELECT CORREO FROM USUARIOS WHERE CORREO = '{$correo}'";
+    #consultamos en la BD si ya existe el correo e
+    $resultado = mysqli_num_rows(mysqli_query($db, $query));
+
+    if($resultado>1){
+        $errores[] = "Ya hay una cuenta asociada con el correo electronico que intentas usar";
     }
+
+    if(!$ciudad){
+        $errores[] = "Ingresa tu ciudad";
+    }
+    if(!$correo){
+        $errores[] = "El email es obligatorio";
+    }
+
+    if(!$contrasenia){
+        $errores[] = "Ingresa una contraseña";    
+    }
+
+    if(!$nombre){
+        $errores[] = "Ingresa tu nombre";        
+    }
+    
+    if(!$apPaterno){
+        $errores[] = "Ingresa tu apellido paterno";
+    }
+
+    if(!$apMaterno){
+        $errores[] = "Ingresa tu apellido materno";
+    }
+
+    if(!$telefono){
+        $errores[] = "Ingresa tu Numero de celular";
+    }
+
+    if(!$estado){
+        $errores[] = "Ingresa tu estado";        
+    }
+    
+    if(!$colonia){
+        $errores[] = "Ingresa tu colonia";                
+    }
+
+    if(!$calle){
+        $errores[] = "Ingresa tu calle";
+    }
+
+    if(!$numero){
+        $errores[] = "Ingresa el numero de casa";
+    }
+
+    if(!$cp){
+        $errores[] = "Ingresa el código postal";
+    }
+
+    if(!$referencias){
+        $errores[] = "Ingresa las referencias";
+    }
+
+    #si el arreglo de errores esta vacio, entonces:
+    if(empty($errores)){
+        
+        #primero hasheamos la contraseña
+        $passwordHash = password_hash($contrasenia, PASSWORD_DEFAULT);               
+        #prepararamos la consulta
+        $query = "INSERT INTO usuarios(NOMBRE, APELLIDO_P, APELLIDO_M, CORREO, TELEFONO, CONTRASENIA, PAIS, ESTADO, CIUDAD, COLONIA, CALLE, NUMERO, REFERENCIAS) 
+                  VALUES ('{$nombre}', '{$apPaterno}', '{$apMaterno}', '{$correo}', '{$telefono}', '{$passwordHash}', '{$pais}', '{$estado}', '{$ciudad}', '{$colonia}', {$calle}, {$numero}, {$referencias})";
+        #procedemos a insertar en la BD
+        $resultado = mysqli_query($db, $query);
+        
+        if($resultado){
+            
+           header('Location: /login.php');
+        }
+        
+    } 
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -35,8 +138,8 @@
 <body>
     <header class="registro-headerprincipal">
         <div class="contenedor">
-            <div class="barra">               
-                    <img class="logo_imagen invertir centrar" src="/Imagenes/logoNegro.png" alt="">
+            <div class="barra">
+                <img class="logo_imagen invertir centrar" src="/Imagenes/logoNegro.png" alt="">
             </div>
         </div>
         <div class="header__texto">
@@ -44,42 +147,65 @@
         </div>
     </header>
     <div>
+        <h1 class="centrar-texto">Registrarse</h1>
+
+        <?php foreach($errores as $error):?>
+            <div class="alerta error centrar-texto">
+                <?php
+                    echo $error;
+                ?>
+            </div>
+        <?php endforeach;?>
+
         <main>
             <form action="" class="registro-dividirFormulario" method="POST">
                 <div class="registro-formulario">
+
+                    <label for="">Correo</label>
+                    <input type="email" placeholder="Tu correo" name="correo" value="<?php echo $correo;?>">
+
+                    <label for="">Contraseña</label>
+                    <input type="password" placeholder="Tu contraseña" name="contrasenia" value="<?php echo $contrasenia;?>">
+
                     <label for="">Nombre</label>
-                    <input type="text" placeholder="tu nombre" name="name">
-                    <label for="">Apellido Materno</label>
-                    <input type="text" placeholder="Tu apellido Materno" name="apMaterno">
-                    <label for="">Telefono</label>
-                    <input type="tel" placeholder="Tu telefono" name="telefono">
-                    <label for="">Estado</label>
-                    <input type="text" placeholder="Tu estado" name="estado">
-                    <label for="">Ciudad</label>
-                    <input type="text" placeholder="Tu ciudad" name="ciudad">
-                    <label for="">Calle</label>
-                    <input type="text" placeholder="Tu calle" name="calle">
-                    <label for="">Referencias</label>
-                    <input type="text" placeholder="Ingresar referencias" name="referencias">
+                    <input type="text" placeholder="tu nombre" name="name" value="<?php echo $nombre;?>">
+
+                    <label for="">Apellido Paterno</label>
+                    <input type="text" placeholder="Tu apellido Paterno" name="apPaterno" value="<?php echo $apPaterno;?>">
                     
+                    <label for="">Apellido Materno</label>
+                    <input type="text" placeholder="Tu apellido Materno" name="apMaterno" value="<?php echo $apMaterno;?>">
+
+                    <label for="telefono">Telefono</label>
+                    <input type="tel" name="telefono" id="telefono" placeholder="Tu numero de telefono" value="<?php echo $telefono;?>">
+
+                    <label for="pais">Pais</label>
+                    <select name="pais" id="pais">
+                        <option value="Mexico">Mexico</option>
+                    </select>
                 </div>
                 <div class="registro-formulario">
-                    <label for="">Apellido Paterno</label>
-                    <input type="text" placeholder="Tu apellido Paterno" name="apPaterno">
-                    <label for="">Correo</label>
-                    <input type="email" placeholder="Tu correo" name="correo">
-                    <label for="">Pais</label>
-                    <select name="pais" id="">
-                        <option value="">Mexico</option>                        
-                    </select>
+                    <label for="">Estado</label>
+                    <input type="text" placeholder="Tu estado" name="estado" value="<?php echo $estado;?>">
+                    
+                    <label for="">Ciudad</label>
+                    <input type="text" placeholder="Tu ciudad" name="ciudad" value="<?php echo $ciudad;?>">
+
                     <label for="">Codigo postal</label>
-                    <input type="text" placeholder="Tu codigo postal" name ="cp">
+                    <input type="text" placeholder="Tu codigo postal" name="cp" value="<?php echo $cp;?>">
+
+                    <label for="">Calle</label>
+                    <input type="text" placeholder="Tu calle" name="calle" value="<?php echo $calle;?>">                    
+
                     <label for="">Colonia</label>
-                    <input type="text" placeholder="Tu colonia" name="colonia">
+                    <input type="text" placeholder="Tu colonia" name="colonia" value="<?php echo $colonia;?>">
+
                     <label for="">Numero</label>
-                    <input type="text" placeholder="Tu numero" name = "numero">
-                    <label for="">Contraseña</label>
-                    <input type="password" placeholder="Tu contraseña" name="contrasenia">
+                    <input type="text" placeholder="Numero" name="numero" value="<?php echo $numero;?>">
+
+                    <label for="">Referencias</label>
+                    <input type="text" placeholder="Ingresar referencias" name="referencias" value="<?php echo $referencias;?>">
+
                     <button class="boton">Registrarse</button>
 
 
@@ -96,4 +222,5 @@
     </footer>
 
 </body>
+
 </html>
