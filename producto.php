40c216287a6c5fd95bb2cabacaf4dff501840a17
dan <diazdaniel422@gmail.com>
2021-05-08 13:18:25 -0500

    Revert "Avance"
    This reverts commit e7480c2604f3e7b65d1923c65eb5d8ca3a4d39f0.

--- producto.php
+++ producto.php
@@ -5,29 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/Css/estilos.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
     <title>Producto</title>
 </head>
 <body>
     <header>
+
+
         <a href="index.php"></a>
         <div class="menu-titulo">
             <h1>Producto</h1>
-            <div class="menu-header-nav">            
-                <nav class="menu-nav">
-                    <button type="button" id="publicar">Publicar un producto</button>
-                    <button type="button" id="misProductos">Mis productos</button>
-                    <!-- <button type="button" id="ventas">Ventas</button>
-                    <button type="button" id="historial">Historial</button>-->
-                </nav>
-            </div>
-        </div>        
-    </header>
-    <main>
-        <div id="contenido-secundario">
         </div>
-    </main>
 
-    <script src="JS/scripts.js"></script>
+
+    </header>
 </body>
 </html>
