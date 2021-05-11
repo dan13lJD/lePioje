52f794b62a4f2c9f0864155978495998dc5717ed
stuartsomera <stuartsomera@gmail.com>
2021-05-11 09:49:28 -0500

    pagina "Nosotros"
    dsd

---
+++ nosotros.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Css/Normalice.css">
+    <link rel="stylesheet" href="Css/estilos.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Antonio:wght@200&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"> 
+    <title>Document</title>
+</head>
+<body>    
+    <header class="nosotros-headerprincipal">
+        <div class="contenedor">
+            <div class="barra">
+                <a class="logo" href="index.html">
+                    <img class="logo_imagen" src="/Imagenes/Logo.png" alt="">
+                </a>   
+            </div>
+            <div class="header__texto">
+                <h2 class="no-margin header__texto__h2">SOBRE NOSOTROS</h2>
+            </div>
+        </div>
+    </header>
+<div class="contenedor">
+    
+    <main>
+        <div class="nosotros-dividircontenido">
+            
+            <img src="Imagenes/nosotros2.jpeg" alt="">
+            
+            <div class="nosotros-contenido">
+                <h2>¿QUIENES SOMOS?</h2>
+                <p>Somos una empresa mexicana en crecimiento sin fines de lucro que nacio mediante un proyecto
+                    dentro de la Universidad Autonoma del Estado de Mexico, el proyecto fue desarrollado por los estudiantes
+                de ingenieria en software: Kevin Stuart Somera Rodriguez y Daniel Jimenez Diaz </p>
+            </div>
+        </div>
+        <div class="nosotros-contenido">
+            <h2>LE PIOJE</h2>
+            <img src="Imagenes/nosotros3.jpeg" alt="" class="nosotros-contenido-imagen">
+            <p>Le Pioje es un proyecto enfocado a la donación de ropa de primer y segunda mano, el objetivo de este proyecto es ofrecer una plataforma web donde cualquier persona  pueda donar ropa que ya no usa o que jamás usó. La motivación para hacer este proyecto surgió a partir de la crisis sanitaria del COVID-19, donde muchas personas se vieron envueltas en problemas económicos a causa de perdida de empleos u otros factores.
+            </p>
+        </div>
+
+    </main>
+</div>
+<div class="redes">
+    <img src="Imagenes/instagram.png" alt="">
+    <img src="Imagenes/facebook.png" alt="">
+</div>
+<div class="redes-nombre">
+    <p>@lePioje</p>
+</div>
+<footer class="footer">
+    <div class="footer-texto">
+        <p>La mejor ropa de segunda mano en el mercado</p>
+    </div>
+    
+</footer>
+</body>
+</html>
